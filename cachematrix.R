8267f7addb6f09f835af9100d071b4515f3ab10f
Omar Elashkar <o_elashkarhotmail.com>
2019-02-05 11:05:23 +0200

    assignment two on coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions will get a square matrix and cache it in makeCacheMatrix, then get it and calculate the inverseinverse.gaussian()
 
-## Write a short comment describing this function
+## This function will get a matrix, and cache it.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      makeCacheMatrix <- function(x = matrix()) {
+            m <- NULL
+            set <- function(y) {
+                  x <<- y
+                  m <<- NULL
+            }
+            get <- function() x
+            setinverse <- function(inverse) m <<- inverse
+            getinverse <- function() m
+            list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+      }
 }
 
 
-## Write a short comment describing this function
+## this function will get the cache of square matrix and calculate its inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      cacheSolve <- function(x, ...) {
+            m <- x$getinverse()
+            if(!is.null(m)) {
+                  message("getting cached data")
+                  return(m)
+            }
+            data <-  x$get()
+            m <- solve(data)
+            x$setinverse(m)
+            m
+      }
 }
